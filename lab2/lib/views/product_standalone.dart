a036b7a1ff99cfa5be58edcda2e8f21bbc48cb5d
Nerphist <nikolailgv@ukr.net>
2020-12-01 13:16:03 +0200

    animations added

--- lab2/lib/views/product_standalone.dart
+++ lab2/lib/views/product_standalone.dart
@@ -1,13 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:lab2/models/product_model.dart';
 
-class ProductStandalone extends StatelessWidget {
+class ProductStandalone extends StatefulWidget {
   static const routeName = '/productStandalone';
+
+  @override
+  _ProductStandaloneState createState() => _ProductStandaloneState();
+}
+
+class _ProductStandaloneState extends State<ProductStandalone>
+    with TickerProviderStateMixin {
+  Animation _priceAnimation, _heartAnimation;
+  AnimationController _priceAnimationController, _heartAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _priceAnimationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _priceAnimation =
+        Tween(begin: 0.0, end: pi).animate(_priceAnimationController);
+
+    _heartAnimationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1200));
+    _heartAnimation = Tween(begin: 150.0, end: 170.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _heartAnimationController));
+
+    _heartAnimationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        _heartAnimationController.repeat();
+      }
+    });
+
+    _priceAnimationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        _priceAnimationController.repeat();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final ProductModel model = ModalRoute.of(context).settings.arguments;
-
+    _priceAnimationController.forward();
     return Scaffold(
         body: Center(
       child: Container(
@@ -28,7 +65,12 @@ class ProductStandalone extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            Text('\$' + model.price.toString()),
+            AnimatedBuilder(
+              animation: _priceAnimationController,
+              builder: (context, child) => Transform.rotate(
+                  angle: _priceAnimation.value,
+                  child: Text('\$' + model.price.toString())),
+            ),
             GestureDetector(
               onTap: () {
                 Navigator.pop(context);
@@ -50,9 +92,55 @@ class ProductStandalone extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            beatingHeart()
           ],
         ),
       ),
     ));
+  }
+
+  Widget beatingHeart() {
+    String animationText = 'Start heart';
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Expanded(
+          child: AnimatedBuilder(
+            animation: _heartAnimationController,
+            builder: (context, child) {
+              return Center(
+                child: Container(
+                  child: Center(
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                      size: _heartAnimation.value,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 12.0),
+            child: OutlineButton(
+              padding: const EdgeInsets.all(12.0),
+              color: Colors.white,
+              textColor: Colors.black,
+              child: Text(animationText),
+              onPressed: () {
+                _heartAnimationController.forward();
+              },
+              splashColor: Colors.red,
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
